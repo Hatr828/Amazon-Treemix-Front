f50f20ce20d8a7ff9e9ff312e7a2fcf5fc3595a0
Leksiyy <leksiy197618@gmail.com>
2026-01-24 04:27:53 +0300

    only html header and some svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import {Header} from "@/widgets/header";
+
 export default function Home() {
   return (
     <div>
       <h1>Test</h1>
+        <Header></Header>
     </div>
   );
 }
---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,62 @@
+﻿import { useHeader } from '../model/useHeader';
+import '../css/header.css'
+
+export function Header() {
+    // const {user, onLogout, menuItems} = useHeader();
+    return (
+        <header>
+            <div>
+                <img src="/public/headerLogo.svg" alt="Logo"/>
+                <button>
+                    <p>Deliver to</p>
+                    <p>Ukraine</p>
+                </button>
+                <form role="search">
+                    <div>All</div>
+                    <input
+                        id="search"
+                        name="q"
+                        type="search"
+                    />
+                    <button type='submit'>
+                        <img src="/public/Search.svg" alt="search"/>
+                    </button>
+
+                </form>
+                <button>
+                    <p>Hello, Sign in</p>
+                    <p>Account</p>
+                </button>
+                <button>
+                    <p>Returns</p>
+                    <p> & Orders</p>
+                </button>
+
+                <button>
+                    <div>2</div>
+                    <img src="/shopping-cart.svg" alt="shopping cart"/>
+                </button>
+            </div>
+            <div>
+                <button>
+                    <img src="/public/more.svg" alt="more.."/>
+                </button>
+                <a href="">
+                    <p>Today's Deals</p>
+                </a>
+                <a href="">
+                    <p>Customer Service</p>
+                </a>
+                <a href="">
+                    <p>Registry</p>
+                </a>
+                <a href="">
+                    <p>Gift Cards</p>
+                </a>
+                <a href="">
+                    <p>Sell</p>
+                </a>
+            </div>
+        </header>
+    )
+}
